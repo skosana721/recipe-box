a6482d76b59e16efa12ac6c4fe6029e6d6f5a062
skosana721 <wonganiskosana@gmail.com>
2021-05-25 10:28:49 +0200

    created a method for handleChange and passed it to the input and textarea

--- recipe/client/src/components/RecipeForm.js
+++ recipe/client/src/components/RecipeForm.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function RecipeForm() {
+  const [formData, setFormData] = useState({
+    recipe: "",
+    ingredients: [],
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
   return (
     <div>
       <form>
-        <input type="text" />
-        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <input
+          type="text"
+          onChange={handleChange}
+          placeholder="Enter recipe "
+        />
+        <textarea
+          name=""
+          id=""
+          onChange={handleChange}
+          cols="30"
+          rows="7"
+          placeholder="Enter ingredients"
+        ></textarea>
         <button type="submit">Add</button>
       </form>
     </div>
