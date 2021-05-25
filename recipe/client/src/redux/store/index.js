7ab7df7c01dd7e07522a4b0d33fd167301232c66
skosana721 <wonganiskosana@gmail.com>
2021-05-25 11:55:33 +0200

    imported the redux store and the provider and wrapped the app component with provider and passed the store as a prop

--- recipe/client/src/redux/store/index.js
+++ recipe/client/src/redux/store/index.js
@@ -1,9 +1,9 @@
-import { createStore, applyMiddleWare, compose } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { recipeReducer } from "../reducer/recipe";
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(
   recipeReducer,
-  composeEnhancer(applyMiddleWare(thunk))
+  composeEnhancer(applyMiddleware(thunk))
 );
