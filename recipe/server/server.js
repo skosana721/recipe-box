3255eb4ea79ea62e6f7af7af7aa62ce155f95251
skosana721 <wonganiskosana@gmail.com>
2021-05-25 11:26:51 +0200

    required the mongoose and connected the express with mongoDB

--- recipe/server/server.js
+++ recipe/server/server.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.listen(4000, () => console.log(`Server is running on port 4000...`));
