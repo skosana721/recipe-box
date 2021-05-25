21279810bed182f71501eef0ca301df99e2b05d6
skosana721 <wonganiskosana@gmail.com>
2021-05-25 10:10:33 +0200

    created a RecipeForm component with one input and textarea with button for add

---
+++ recipe/client/src/components/RecipeForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function RecipeForm() {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default RecipeForm;
