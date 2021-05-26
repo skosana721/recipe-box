f28876630649352d79a9ce26573967e607f0c4dc
skosana721 <wonganiskosana@gmail.com>
2021-05-26 11:18:34 +0200

    created a post request

--- recipe/server/routes/api/recipe.js
+++ recipe/server/routes/api/recipe.js
@@ -4,5 +4,17 @@ const recipeRoutes = (app) => {
   app.get("/api/recipe", (req, res) => {
     res.send("hello");
   });
+  app.post("/api/recipe", async (req, res) => {
+    const { recipe, ingredients } = req.body;
+    try {
+      const newRecipe = await Recipe({
+        recipe,
+        ingredients,
+      }).save();
+      res.status(201).send(newRecipe);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  });
 };
 module.exports = { recipeRoutes };
