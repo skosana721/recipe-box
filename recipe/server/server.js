834bb3293d57eac9a5ab422705961e92cce51c90
skosana721 <wonganiskosana@gmail.com>
2021-05-26 10:36:03 +0200

    requred cors and use and passed to a middleware

--- recipe/server/server.js
+++ recipe/server/server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv/config");
+const cors = require("cors");
 const { recipeRoutes } = require("./routes/api/recipe");
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
+app.use(cors());
 recipeRoutes(app);
 app.listen(4000, () => console.log(`Server is running on port 4000...`));
