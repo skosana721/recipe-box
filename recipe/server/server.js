3edffb7daa224d5c8520a27f29476e92fad7ee7e
skosana721 <wonganiskosana@gmail.com>
2021-05-26 10:05:15 +0200

    import the recipeRoutes in the server.js and the called the function and pass add as an argument

--- recipe/server/server.js
+++ recipe/server/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv/config");
+const { recipeRoutes } = require("./routes/api/recipe");
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+recipeRoutes(app);
 app.listen(4000, () => console.log(`Server is running on port 4000...`));
