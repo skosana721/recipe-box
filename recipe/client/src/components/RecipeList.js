4a829507f3fd5ec42c5903c3daa87958c7c677c0
skosana721 <wonganiskosana@gmail.com>
2021-05-27 11:17:36 +0200

    selected recipeList from redux store with useSelector and mapped through the recipeList and returned the recipe component and passed data as props

--- recipe/client/src/components/RecipeList.js
+++ recipe/client/src/components/RecipeList.js
@@ -4,8 +4,13 @@ import Recipe from "./Recipe";
 
 function RecipeList() {
   const recipeList = useSelector((state) => state.recipeList);
-  console.log(recipeList);
-  return <div></div>;
+  return (
+    <div>
+      {recipeList.map((recipe) => {
+        return <Recipe key={recipe._id} {...recipe} />;
+      })}
+    </div>
+  );
 }
 
 export default RecipeList;
