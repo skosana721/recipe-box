127687e5472d35202c294883657fc469915360d0
skosana721 <wonganiskosana@gmail.com>
2021-05-27 11:58:02 +0200

    added onClick to the delete which dispatch deleteRecipe with an id

--- recipe/client/src/components/Recipe.js
+++ recipe/client/src/components/Recipe.js
@@ -11,14 +11,15 @@ import {
 } from "reactstrap";
 function Recipe({ recipe, _id, ingredients }) {
   const [isOpen, setIsOpen] = useState(false);
-
   const toggle = () => setIsOpen(!isOpen);
+  const dispatch = useDispatch();
+
   return (
     <div>
       <ListGroup>
         <ListGroupItem>
           <span onClick={toggle}>{recipe}</span>
-          <Button>Delete</Button>
+          <Button onClick={() => dispatch(deleteRecipe(_id))}>Delete</Button>
         </ListGroupItem>
         <Collapse isOpen={isOpen}>
           <Card>
