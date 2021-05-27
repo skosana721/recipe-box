482873e6edbe7af37e4f7951f8c92d80481cd191
skosana721 <wonganiskosana@gmail.com>
2021-05-27 11:55:42 +0200

    imported the useDispatch

--- recipe/client/src/components/Recipe.js
+++ recipe/client/src/components/Recipe.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { deleteRecipe } from "../redux/actions/recipe";
+import { useDispatch } from "react-redux";
 import {
   ListGroup,
   ListGroupItem,
