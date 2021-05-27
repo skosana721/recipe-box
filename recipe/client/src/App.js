b81c4cb82d730b2f2b812ff9c3d8199b4b5a5ef8
skosana721 <wonganiskosana@gmail.com>
2021-05-27 11:16:03 +0200

    rendred the RecipeList component

--- recipe/client/src/App.js
+++ recipe/client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import RecipeForm from "./components/RecipeForm";
+import RecipeList from "./components/RecipeList";
 
 function App() {
   return (
     <div className="App">
       <RecipeForm />
+      <RecipeList />
     </div>
   );
 }
