4e2b60ec757d8597410c08788cb4d8f63fb30292
skosana721 <wonganiskosana@gmail.com>
2021-05-27 10:11:16 +0200

    imported useEffect

--- recipe/client/src/components/RecipeForm.js
+++ recipe/client/src/components/RecipeForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addRecipe } from "../redux/actions/recipe";
 
