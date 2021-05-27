4bfc0ce6e1f3ba3af8a165a9888ead04f1207b6d
skosana721 <wonganiskosana@gmail.com>
2021-05-27 09:34:00 +0200

    add a case for ADD_RECIPE

--- recipe/client/src/redux/reducer/recipe.js
+++ recipe/client/src/redux/reducer/recipe.js
@@ -1,9 +1,13 @@
+import { ADD_RECIPE } from "../actionTypes/recipe";
+
 const initialState = {
   recipeList: [],
 };
 
 export const recipeReducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_RECIPE:
+      return { ...state, recipeList: [...state.recipeList, action.payload] };
     default:
       return state;
   }
