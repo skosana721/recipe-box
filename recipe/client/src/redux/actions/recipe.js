349054adbef2c9cde170129b9b9f20fe3ac4b674
skosana721 <wonganiskosana@gmail.com>
2021-05-27 09:35:52 +0200

    added an action for getRecipe which makes an axios get request to the server

--- recipe/client/src/redux/actions/recipe.js
+++ recipe/client/src/redux/actions/recipe.js
@@ -1,9 +1,19 @@
 import axios from "axios";
-import { ADD_RECIPE } from "../actionTypes/recipe";
+import { ADD_RECIPE, GET_RECIPES } from "../actionTypes/recipe";
 
+export const getRecipe = () => {
+  return (dispatch) => {
+    axios.get("http://localhost:4000/api/recipe").then((res) =>
+      dispatch({
+        type: GET_RECIPES,
+        payload: res.data,
+      })
+    );
+  };
+};
 export const addRecipe = (recipe) => {
   return (dispatch) => {
-    axios.post("http://localhost:4000/api/recipe", { recipe }).then((res) =>
+    axios.post("http://localhost:4000/api/recipe", recipe).then((res) =>
       dispatch({
         type: ADD_RECIPE,
         payload: res.data,
