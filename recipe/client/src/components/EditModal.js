a553e0574aee435313829468227dddc600cd6af8
skosana721 <wonganiskosana@gmail.com>
2021-05-28 11:17:54 +0200

    created a Modal whcih initial has hello world text in the ModalBody

--- recipe/client/src/components/EditModal.js
+++ recipe/client/src/components/EditModal.js
@@ -2,7 +2,21 @@ import React from "react";
 
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 function EditModal() {
-  return <div></div>;
+  return (
+    <div>
+      <Button color="danger">
+        <h3>Edit</h3>
+      </Button>
+      <Modal>
+        <ModalHeader>Modal title</ModalHeader>
+        <ModalBody>Hello World</ModalBody>
+        <ModalFooter>
+          <Button color="primary">Do Something</Button>{" "}
+          <Button color="secondary">Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
 }
 
 export default EditModal;
