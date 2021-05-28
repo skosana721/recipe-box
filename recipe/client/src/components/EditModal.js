66443c01be7bd04e73d31c67b792a4f373115c39
skosana721 <wonganiskosana@gmail.com>
2021-05-28 11:59:59 +0200

    added handleSubmit method and passed it on the onSubmit in the form

--- recipe/client/src/components/EditModal.js
+++ recipe/client/src/components/EditModal.js
@@ -5,6 +5,9 @@ function EditModal({ ingredients }) {
   const [modal, setModal] = useState(false);
   const [newIngredients, setNewIngredients] = useState(ingredients);
   const toggle = () => setModal(!modal);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
       <Button color="danger" onClick={toggle}>
@@ -13,7 +16,7 @@ function EditModal({ ingredients }) {
       <Modal isOpen={modal} toggle={toggle}>
         <ModalHeader>Modal title</ModalHeader>
         <ModalBody>
-          <form>
+          <form onSubmit={handleSubmit}>
             <input
               type="text"
               onChange={(e) => setNewIngredients(e.target.value)}
