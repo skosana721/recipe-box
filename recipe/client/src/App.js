32c06386849841ab3bb89962c743462ec4a00c3f
skosana721 <wonganiskosana@gmail.com>
2021-06-03 10:09:57 +0200

    renderd the NavBar component

--- recipe/client/src/App.js
+++ recipe/client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 import RecipeForm from "./components/RecipeForm";
 import RecipeList from "./components/RecipeList";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <RecipeForm />
       <RecipeList />
     </div>
